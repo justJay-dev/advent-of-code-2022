0a6bb9331e461611213147c9eaff42249f810641
justJay-dev <jay@lowbrowlabs.com>
2022-12-03 12:38:15 -0500

    day threeeeee

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -1,4 +1,60 @@
-def main():
+letter_priority = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+}
+
+
+def part1():
     total_priority = 0
     with open("day-3/input.txt") as f:
         # split each line in half
@@ -13,64 +69,27 @@ def main():
                 set(second_compartment)
             )
 
-            letter_priority = {
-                "a": 1,
-                "b": 2,
-                "c": 3,
-                "d": 4,
-                "e": 5,
-                "f": 6,
-                "g": 7,
-                "h": 8,
-                "i": 9,
-                "j": 10,
-                "k": 11,
-                "l": 12,
-                "m": 13,
-                "n": 14,
-                "o": 15,
-                "p": 16,
-                "q": 17,
-                "r": 18,
-                "s": 19,
-                "t": 20,
-                "u": 21,
-                "v": 22,
-                "w": 23,
-                "x": 24,
-                "y": 25,
-                "z": 26,
-                "A": 27,
-                "B": 28,
-                "C": 29,
-                "D": 30,
-                "E": 31,
-                "F": 32,
-                "G": 33,
-                "H": 34,
-                "I": 35,
-                "J": 36,
-                "K": 37,
-                "L": 38,
-                "M": 39,
-                "N": 40,
-                "O": 41,
-                "P": 42,
-                "Q": 43,
-                "R": 44,
-                "S": 45,
-                "T": 46,
-                "U": 47,
-                "V": 48,
-                "W": 49,
-                "X": 50,
-                "Y": 51,
-                "Z": 52,
-            }
             # print the sum of the common letters for each ruck sack
             total_priority += sum([letter_priority[x] for x in common_letters])
 
     print(total_priority)
 
 
-main()
+def part2():
+    total_priority = 0
+    with open("day-3/input.txt") as f:
+        data = f.read().split("\n")
+        # group data into 3s
+        data = [data[i : i + 3] for i in range(0, len(data), 3)]
+        # for each group of 3
+        for group in data:
+            # find the common letters between the 3 lines
+            common_letters = set(group[0]).intersection(set(group[1]), set(group[2]))
+            # print the sum of the common letters for each ruck sack
+            total_priority += sum([letter_priority[x] for x in common_letters])
+
+    print(total_priority)
+
+
+part1()
+part2()
